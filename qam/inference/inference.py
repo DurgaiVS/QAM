7c4e8d0dc60cb5414b5a6b8caca3b35f79943644
DurgaiVS <durgaivel0309@gmail.com>
2024-11-10 11:52:27 +0530

    TEMP commit

--- qam/inference/inference.py
+++ qam/inference/inference.py
@@ -133,3 +133,201 @@ class QAMDatasetPredictor(pl.LightningModule):
     @rank_zero_only
     def get_metrics(self) -> QAMOverallDataMetric:
         return QAMOverallDataMetric.read_from(self.writer.overall_summary_file)
+
+
+####################################################################################################
+
+from typing import Dict, Tuple, Union
+
+import pytorch_lightning as pl
+import torch
+from omegaconf import DictConfig
+from onnxruntime import InferenceSession
+from pytorch_lightning.utilities import rank_zero_only
+from registrable import Registrable
+from torchmetrics import ConfusionMatrix, StatScores
+from zspeech.common import defaultdict
+from zspeech.itn.common_utils import (
+    PAD_LABEL_ID,
+    CapitLabel,
+    ITNDataBatch,
+    ITNDataSample,
+    ITNOverallDataMetric,
+    PunctLabel,
+)
+from zspeech.itn.models import PunctuationCapitalisationModel
+
+from .utils import ITNInferenceResultsWriter
+
+
+class ITNBasePredictor(pl.LightningModule, Registrable):
+    def __init__(
+        self,
+        model: Union[PunctuationCapitalisationModel, str, InferenceSession],
+        output_dir: str,
+        punct_labels: int = len(PunctLabel.__members__),
+        capit_labels: int = len(CapitLabel.__members__),
+        f_beta: float = 1.0,
+    ):
+        super().__init__()
+        self.model = model
+        self.output_dir = output_dir
+        # f_beta: can give manual weightage to precision (and/or) recall
+        self.f_beta = f_beta
+        self.punct_labels = punct_labels
+        self.capit_labels = capit_labels
+        # macro: computes score for individual classes and then return avg of them
+        # none: computes score for individual classes and return as a list
+        self.dataset_wise_statscore = defaultdict(
+            lambda: {
+                f"{catg}_stats": StatScores(
+                    task="multiclass",
+                    num_classes=getattr(self, f"{catg}_labels"),
+                    average="none",
+                ).to(self.device)
+                for catg in self.categories
+            }
+        )
+
+        self.dataset_wise_confmat = defaultdict(
+            lambda: {f"{catg}_confmat": 0 for catg in self.categories}
+        )
+
+        self.dataset_wise_labelweight = defaultdict(
+            lambda: {f"{catg}_weights": defaultdict(lambda: 0) for catg in self.categories}
+        )
+
+        self.categories = ["punct", "capit"]
+        self.metrics_name = ["f1score", "precision", "recall"]
+        self.writer = ITNInferenceResultsWriter(
+            self.output_dir,
+            self.categories,
+            self.metrics_name,
+            [self.f1score, self.precision, self.recall],
+        )
+
+        for catg in self.categories:
+            setattr(
+                self,
+                f"{catg}_confusion_matrix",
+                ConfusionMatrix(
+                    task="multiclass",
+                    num_classes=getattr(self, f"{catg}_labels"),
+                ),
+            )
+
+    def f1score(self, tp: torch.Tensor, fp: torch.Tensor, fn: torch.Tensor) -> torch.Tensor:
+        return ((1 + (self.f_beta**2)) * tp) / (
+            ((1 + (self.f_beta**2)) * tp) + fp + ((self.f_beta**2) * fn)
+        )
+
+    def precision(self, tp: torch.Tensor, fp: torch.Tensor, fn: torch.Tensor) -> torch.Tensor:
+        return tp / (tp + fp)
+
+    def recall(self, tp: torch.Tensor, fp: torch.Tensor, fn: torch.Tensor) -> torch.Tensor:
+        return tp / (tp + fn)
+
+    def on_predict_start(self):
+        for catg in self.categories:
+            setattr(
+                self,
+                f"{catg}_confusion_matrix",
+                getattr(self, f"{catg}_confusion_matrix").to(self.device),
+            )
+
+    def on_predict_epoch_end(self) -> None:
+        self.writer.write_overall_stats(
+            self.dataset_wise_statscore, self.dataset_wise_confmat, self.dataset_wise_labelweight
+        )
+
+    @rank_zero_only
+    def get_metrics(self) -> ITNOverallDataMetric:
+        return ITNOverallDataMetric.read_from(self.writer.overall_summary_file)
+
+    def on_predict_batch_end(
+        self,
+        outputs: Tuple[torch.Tensor, torch.Tensor],
+        batch: ITNDataBatch,
+        batch_idx: int,
+        dataloader_idx: int = 0,
+    ) -> None:
+        punct_logits, capit_logits = outputs
+
+        gl = globals()
+        punct_preds = torch.argmax(punct_logits, dim=2)
+        capit_preds = torch.argmax(capit_logits, dim=2)
+
+        for (
+            sample,
+            punct_pred,
+            capit_pred,
+        ) in zip(
+            batch,
+            punct_preds,
+            capit_preds,
+        ):
+            sample: ITNDataSample
+            mask = sample.punct_labels != PAD_LABEL_ID
+            punct_label = sample.punct_labels[mask].unsqueeze(0)
+            punct_pred = punct_pred[mask].unsqueeze(0)
+            capit_label = sample.capit_labels[mask].unsqueeze(0)
+            capit_pred = capit_pred[mask].unsqueeze(0)
+
+            ll = locals()
+            score_dict = {}
+            ds_name = sample.dataset_name
+
+            for catg in self.categories:
+                ll[f"{catg}_score"] = self.dataset_wise_statscore[ds_name][f"{catg}_stats"](
+                    ll[f"{catg}_pred"], ll[f"{catg}_label"]
+                )
+
+                score = getattr(self, f"{catg}_confusion_matrix")(
+                    ll[f"{catg}_pred"], ll[f"{catg}_label"]
+                )
+                self.dataset_wise_confmat[ds_name][f"{catg}_confmat"] += score
+                score_dict[f"{catg}_confmat"] = score.cpu().tolist()
+
+                ll[f"{catg}_unique"], unique_count = ll[f"{catg}_label"].unique(return_counts=True)
+                for id, (c_l, c_c) in enumerate(zip(ll[f"{catg}_unique"], unique_count)):
+                    if c_l.item() == gl.get(f"{catg.capitalize()}Label").NoOp.value:
+                        unique_count[id] = 0
+                        # since the tensors are passing reference, this will make the
+                        # `c_c` value `0`
+
+                    self.dataset_wise_labelweight[ds_name][f"{catg}_weights"][c_l.item()] += c_c
+
+                ll[f"{catg}_unique_count"] = unique_count / unique_count.sum()
+
+            for metric in self.metrics_name:  # f1, prec, recall
+                metric_vals = []
+                for catg in self.categories:  # punct, capit
+                    cls = gl.get(f"{catg.capitalize()}Label")
+                    catg_vals = []
+                    for id, (tp, fp, _, fn, _) in enumerate(ll[f"{catg}_score"]):  # labels per catg
+                        val = getattr(self, metric)(tp, fp, fn)
+                        score_dict[f"{catg}_{cls(id).name.lower()}_{metric}"] = val.cpu().item()
+                        if not torch.isnan(val):
+                            pos = torch.where(ll[f"{catg}_unique"] == id)[0]
+                            if len(pos):
+                                catg_vals.append(val * ll[f"{catg}_unique_count"][pos])
+
+                    val = torch.stack(catg_vals).sum()
+                    score_dict[f"{catg}_{metric}"] = val.cpu().item()
+                    metric_vals.append(val)
+
+                score_dict[f"{metric}"] = torch.stack(metric_vals).mean().cpu().item()
+
+            self.writer.write_sample_wise_stats(
+                sample, punct_pred.squeeze(0).cpu(), capit_pred.squeeze(0).cpu(), score_dict
+            )
+
+    def predict_step(
+        self, batch: ITNDataBatch, batch_idx: int, dataloader_idx: int = 0
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        raise NotImplementedError
+
+    @classmethod
+    def from_cfg(cls, cfg: DictConfig) -> Tuple["ITNBasePredictor", Dict]:
+        # to return PT model name (from Huggingface), max_word_per_group, p&c labels count
+        raise NotImplementedError
--- qam/inference/utils.py
+++ qam/inference/utils.py
@@ -206,3 +206,211 @@ def wrap_up_function(
     # for ds_name in dataset_names:
     #     shutil.rmtree(os.path.join(output_dir, ds_name), ignore_errors=True)
     # os.remove(os.path.join(output_dir, "overall_summary.json"))
+
+################################################################################################
+
+import csv
+import os
+
+# import shutil
+import tarfile
+from typing import Callable, Dict, List, Union
+
+import torch
+from omegaconf import DictConfig, OmegaConf
+from torchmetrics.classification import MulticlassStatScores
+from zspeech.common import defaultdict, find_available_filename
+from zspeech.itn.common_utils import (
+    CapitLabel,
+    ITNDataSample,
+    ITNFileWriter,
+    ITNOverallDataMetric,
+    ITNSampleDataMetric,
+    PunctLabel,
+    modify_capit_for_word,
+    modify_punct_for_word,
+)
+
+
+def generate_prediction(
+    sample: ITNDataSample, punct_preds: torch.Tensor, capit_preds: torch.Tensor
+) -> str:
+    output = []
+    # removing special tokens from sample.words
+    # removing except first but all other subtokens for a word using mask for punct & capit preds
+    for word, punct_label, capit_label in zip(
+        sample.words[1:-1], punct_preds[sample.mask], capit_preds[sample.mask]
+    ):
+        word = modify_punct_for_word(word, punct_label)
+        word = modify_capit_for_word(word, capit_label)
+
+        output.append(word)
+
+    return " ".join(output)
+
+
+class ITNInferenceResultsWriter:
+    def __init__(
+        self,
+        output_dir: str,
+        categories: List[str],
+        metrics_name: List[str],
+        metrics_fn: List[Callable[[torch.Tensor, torch.Tensor, torch.Tensor], torch.Tensor]],
+    ) -> None:
+        self.output_dir = output_dir
+        self.overall_summary_file = os.path.join(self.output_dir, "overall_summary.json")
+        self.dataset_wise_sample_stats = defaultdict(self.default_fn, True)
+        self.categories = categories
+        self.metrics_name = metrics_name
+        for m_name, m_fn in zip(metrics_name, metrics_fn):
+            setattr(self, m_name, m_fn)
+
+    def default_fn(self, key: str) -> Dict:
+        ds_path = os.path.join(self.output_dir, key)
+        os.makedirs(ds_path, exist_ok=True)
+        return {
+            "sample_wise_file": ITNFileWriter(
+                full_path=find_available_filename(ds_path, "sample_stats", "jsonl.gz", False),
+                size_per_file=float("inf"),
+            ),
+            "overall_summary": os.path.join(ds_path, "overall_summary.json"),
+        }
+
+    def write_sample_wise_stats(
+        self,
+        sample: ITNDataSample,
+        punct_preds: torch.Tensor,
+        capit_preds: torch.Tensor,
+        score_dict: Dict[str, torch.Tensor],
+    ):
+        self.dataset_wise_sample_stats[sample.dataset_name]["sample_wise_file"].write(
+            ITNSampleDataMetric(
+                ground_truth=sample.ground_truth,
+                dataset_name=sample.dataset_name,
+                prediction=generate_prediction(sample, punct_preds, capit_preds),
+                **score_dict,
+            )
+        )
+
+    def compute_stats(
+        self, scores: Dict[str, torch.Tensor], weights: Dict[str, Dict[int, torch.Tensor]]
+    ) -> Dict[str, Union[float, List]]:
+        stats = {}
+        gl = globals()
+
+        for catg in self.categories:
+            stats[f"{catg}_confmat"] = (
+                (scores[f"{catg}_confmat"] / scores[f"{catg}_confmat"].sum(dim=1)[:, None])
+                .cpu()
+                .tolist()
+            )
+
+            _weights = torch.zeros(
+                len(gl[f"{catg.capitalize()}Label"].__members__), dtype=torch.int64
+            )
+            for k, v in weights[f"{catg}_weights"].items():
+                if k == gl.get(f"{catg.capitalize()}Label").NoOp.value:
+                    continue
+                _weights[k] = v
+
+            weights[f"{catg}_weights"] = _weights / _weights.sum()
+
+        for metric in self.metrics_name:
+            metric_vals = []
+            for catg in self.categories:
+                cls = gl.get(f"{catg.capitalize()}Label")
+                catg_vals = []
+                for id, (tp, fp, _, fn, _) in enumerate(scores[f"{catg}_score"]):
+                    val = getattr(self, metric)(tp, fp, fn)
+                    stats[f"{catg}_{cls(id).name.lower()}_{metric}"] = val.cpu().item()
+                    if not torch.isnan(val):
+                        catg_vals.append(val * weights[f"{catg}_weights"][id])
+
+                # NOTE:
+                # instead of computing sum of tp, fp, fn 's, we've computed weighted avg of labelwise metric
+                # values coz, summing up is messing with the scores, causing F1, Prec, Rec all to be same...
+                # like, FPs == FNs,
+                val = torch.stack(catg_vals).sum()
+                stats[f"{catg}_{metric}"] = val.cpu().item()
+                metric_vals.append(val)
+
+            stats[f"{metric}"] = torch.stack(metric_vals).mean().cpu().item()
+
+        return stats
+
+    def write_overall_stats(
+        self,
+        dataset_wise_statscore: Dict[str, Dict[str, MulticlassStatScores]],
+        dataset_wise_confmat: Dict[str, Dict[str, torch.Tensor]],
+        dataset_wise_labelweight: Dict[str, Dict[str, Dict[int, torch.Tensor]]],
+    ):
+
+        overall = defaultdict(lambda: 0)
+        overall_weight = {f"{catg}_weights": defaultdict(lambda: 0) for catg in self.categories}
+
+        for dataset, stats_metrics_dict in dataset_wise_statscore.items():
+            scores = {}
+            self.dataset_wise_sample_stats[dataset]["sample_wise_file"].close()
+
+            for catg in self.categories:
+                scores[f"{catg}_confmat"] = dataset_wise_confmat[dataset][f"{catg}_confmat"]
+                overall[f"{catg}_confmat"] += scores[f"{catg}_confmat"]
+
+                for c_l, c_c in dataset_wise_labelweight[dataset][f"{catg}_weights"].items():
+                    overall_weight[f"{catg}_weights"][c_l] += c_c
+
+                scores[f"{catg}_score"] = stats_metrics_dict[f"{catg}_stats"].compute()
+                overall[f"{catg}_score"] += scores[f"{catg}_score"]
+
+            ITNOverallDataMetric(
+                **self.compute_stats(scores, dataset_wise_labelweight[dataset])
+            ).write_from_rank_zero_only(self.dataset_wise_sample_stats[dataset]["overall_summary"])
+
+        ITNOverallDataMetric(
+            **self.compute_stats(overall, overall_weight)
+        ).write_from_rank_zero_only(self.overall_summary_file)
+
+
+def generate_csv(output_dir: str, dataset_names: list[str]) -> tuple[str, str]:
+    stats = defaultdict(lambda: [])
+    csv_path = os.path.join(output_dir, "overall_summary.csv")
+    for ds_name in dataset_names:
+        stats["ds_name"].append(ds_name)
+        data = ITNOverallDataMetric.read_from(
+            os.path.join(output_dir, ds_name, "overall_summary.json")
+        )
+
+        for k, v in vars(data).items():
+            if k.startswith("_"):
+                continue
+            stats[k].append(v)
+
+    with open(csv_path, "w", newline="") as f:
+        w = csv.writer(f)
+        w.writerow(stats.keys())
+        w.writerows(zip(*stats.values()))
+
+    return csv_path, "overall_summary.csv"
+
+
+def wrap_up_function(
+    output_dir: str,
+    dataset_names: List[str],
+    model_hparams: DictConfig,
+    predictor_hparams: DictConfig,
+):
+    hparams = OmegaConf.merge(
+        {"model_hparams": model_hparams}, {"predictor_hparams": predictor_hparams}
+    )
+    OmegaConf.save(hparams, os.path.join(output_dir, "overall_hparams.yaml"))
+    csv_path, csv_name = generate_csv(output_dir, dataset_names)
+    with tarfile.open(os.path.join(output_dir, "results.tar.gz"), "w:gz") as tar:
+        for ds_name in dataset_names:
+            tar.add(os.path.join(output_dir, ds_name), arcname=ds_name)
+        tar.add(csv_path, arcname=csv_name)
+        tar.add(os.path.join(output_dir, "overall_summary.json"), arcname="overall_summary.json")
+        tar.add(os.path.join(output_dir, "overall_hparams.yaml"), arcname="overall_hparams.yaml")
+
+    # for ds_name in dataset_names:
+    #     shutil.rmtree(os.path.join(output_dir, ds_name), ignore_errors=True)
+    # os.remove(os.path.join(output_dir, "overall_summary.json"))
